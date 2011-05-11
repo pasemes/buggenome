c66c7838c6cff9a0dce6c2a4d498260a8c671ccd
Paulo Sérgio <pasemes@gmail.com>
2011-05-10 23:14:14 -0300

    implementing the java stack trace matcher

--- exceptionmatcher/src/main/scala/com/buggenome/matcher/JavaStackMatcher.scala
+++ exceptionmatcher/src/main/scala/com/buggenome/matcher/JavaStackMatcher.scala
@@ -3,6 +3,7 @@ package com.buggenome.matcher
 import com.buggenome.stacktrace.StackTrace
 import collection.mutable.ListBuffer
 import util.Sorting
+import scala.Array
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,19 +11,33 @@ import util.Sorting
  * Date: 07/05/11
  * Time: 20:04
  */
-class JavaStackMatcher {
-
-    val similarityCalculators = JavaSimilarityCalculatorFactory.getSimilarityCalculatorsSortedByRelevance
+class JavaStackMatcher(val dependencies : { val similarityCalculators : Array[JavaStackSimilarityCalculator] } ) {
 
     def computeSimilarStacks(stackTrace : StackTrace, stackTraces : List[StackTrace]) : List[StackTrace] = {
         val orderedSimilarStacks = new ListBuffer[StackTrace]()
-        similarityCalculators.foreach{ similarityCalculator =>
+        dependencies.similarityCalculators.foreach{ similarityCalculator =>
             val mappedSimilarity = stackTraces.map{ //maps to a tuple (stackTrace, similarityValue)
                     similarStack => (similarStack, similarityCalculator.computeSimilarity(stackTrace, similarStack))}
             val orderedMappedSimilarity = mappedSimilarity.sortWith{ //sorts the stacks based on the similarityValue
                     (stackSimilarityTuple1, stackSimilarityTuple2) => stackSimilarityTuple1._2 < stackSimilarityTuple2._2}
             orderedSimilarStacks ++= orderedMappedSimilarity.filter(_._2 != 0).map(_._1) //removes the stacks which don't have any degree of similarity and maps it back to a list of stacks
         }
-        return orderedSimilarStacks.toList
+        orderedSimilarStacks.toList
     }
+}
+
+/**
+  * Configures a JavaStackMatcher with similarity calculators sorted by result relevance, i.e., the first calculator used by the matcher
+  *      use the most precise and restrictive algorithm for similarity calculation and the last is the least precise/restrictive.
+  */
+object JavaStackMatcherPrecisionConfig {
+    lazy val similarityCalculators : Array[JavaStackSimilarityCalculator] = {
+        val calculators = new Array[JavaStackSimilarityCalculator](4)
+        calculators(0) = new JavaStackCompleteMatchSimilarityCalculator
+        calculators(1) = new JavaStackChainedSimilarityCalculator
+        calculators(2) = new JavaStackTopDownSimilarityCalculator
+        calculators(3) = new JavaStackBottomUpSimilarityCalculator
+        calculators
+    }
+    lazy val javaStackMatcher : JavaStackMatcher = new JavaStackMatcher(this)
 }
--- exceptionmatcher/src/main/scala/com/buggenome/matcher/JavaStackSimilarityCalculator.scala
+++ exceptionmatcher/src/main/scala/com/buggenome/matcher/JavaStackSimilarityCalculator.scala
@@ -9,15 +9,45 @@ import com.buggenome.stacktrace.StackTrace
  * Time: 17:43
  */
 
-abstract class JavaStackSimilarityCalculator {
+trait JavaStackSimilarityCalculator {
 
     /**
      * Compute the similarity between two stacks is based on the number of common lines.
      * @return an double value from 0 to 1, indicating the similarity
      */
     def computeSimilarity(stackTrace : StackTrace, otherStackTrace : StackTrace) : Double
+}
+
+class JavaStackCompleteMatchSimilarityCalculator extends JavaStackSimilarityCalculator {
+
+    //sempre retorna 1
+    def computeSimilarity(stackTrace : StackTrace, otherStackTrace : StackTrace) : Double = {
+
+    }
+}
+
+class JavaStackTopDownSimilarityCalculator extends JavaStackSimilarityCalculator {
+
+    //exclui complete match (isto e, nunca retorna 1)
+    def computeSimilarity(stackTrace : StackTrace, otherStackTrace : StackTrace) : Double = {
+
+    }
+}
+
+class JavaStackBottomUpSimilarityCalculator extends JavaStackSimilarityCalculator {
+
+    //exclui complete match (isto e, nunca retorna 1)
+    def computeSimilarity(stackTrace : StackTrace, otherStackTrace : StackTrace) : Double = {
+
+    }
+}
 
 
+class JavaStackChainedSimilarityCalculator extends JavaStackSimilarityCalculator {
 
+    //pode dar match "no meio" (em uma stack chained) da stack e nao so no inicio ou no fim
+    def computeSimilarity(stackTrace : StackTrace, otherStackTrace : StackTrace) : Double = {
+
+    }
 
 }
